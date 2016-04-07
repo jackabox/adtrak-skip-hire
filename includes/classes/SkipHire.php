f01724e0ac5e5b715ba958fc7e6c0cbe6794c159
Jack Whiting <jack.whiting@adtrak.co.uk>
2016-04-07 16:31:11 +0100

    form fields implemented

--- includes/classes/SkipHire.php
+++ includes/classes/SkipHire.php
@@ -134,63 +134,212 @@ class ad_skip_hire
         $lat = (isset($_REQUEST['ash_lat'])) ? $_REQUEST['ash_lat'] : NULL; 
         $lng = (isset($_REQUEST['ash_lng'])) ? $_REQUEST['ash_lng'] : NULL; 
         $skip = (isset($_REQUEST['ash_skip_id'])) ? $_REQUEST['ash_skip_id'] : NULL; 
-    ?>
-        <div id="ash_booking" class="ash-booking-holder">
-            <?php if($postcode == null && $lat == null): 
+        
+        if($skip == null && $lat != null): ?>
+            <h3>Choose a SKip</h3>
+            <p>We found the following skips available for delivery in the area of <?php echo strtoupper($postcode); ?>.</p>
 
-                echo "<p>Sorry, we couldn't find your location. Please try again using the form below.</p>";
-                echo do_shortcode('[ash_postcode_form]');
+            <?php
+            // WP_Query arguments
+            $args = [
+                'post_type'              => 'ash_skips',
+                'post_status'            => 'publish',
+                'posts_per_page'         => -1,
+                'cache_results'          => true,
+            ];
 
-            elseif($skip == null): ?>
-                <h3>Choose a SKip</h3>
-                <p>We found the following skips available for delivery in the area of <?php echo strtoupper($postcode); ?>.</p>
-
-                <?php
-                // WP_Query arguments
-                $args = [
-                    'post_type'              => 'ash_skips',
-                    'post_status'            => 'publish',
-                    'posts_per_page'         => -1,
-                    'cache_results'          => true,
-                ];
-
-                $query = new WP_Query( $args );
-                if ( $query->have_posts() ): while ( $query->have_posts() ):
-                    $query->the_post(); ?>
-                    <div class="ash-skip" id="ash-skip-<?php echo get_the_ID(); ?>">
-                        <h3 class="ash-skip-title"><?php the_title(); ?></h3>
-                        <p class="ash-skip-description"><?php echo get_post_meta( get_the_ID(), 'ash_skips_description', true ); ?></p>
-                        <div class="ash-skip-meta">
-                            <span class="ash-skip-meta__width">Width: <?php echo get_post_meta( get_the_ID(), 'ash_skips_width', true ); ?></span>
-                            <span class="ash-skip-meta__height">Height: <?php echo get_post_meta( get_the_ID(), 'ash_skips_height', true ); ?></span>
-                            <span class="ash-skip-meta__length">Length: <?php echo get_post_meta( get_the_ID(), 'ash_skips_length', true ); ?></span>
-                            <span class="ash-skip-meta__capacity">Capacity: <?php echo get_post_meta( get_the_ID(), 'ash_skips_capacity', true ); ?></span>
-                        </div>
-                        <form action="#" method="POST">
-                            <input type="hidden" id="ash_postcode" name="ash_postcode" value="<?php echo $postcode; ?>">
-                            <input type="hidden" id="ash_skip_id" name="ash_skip_id" value="<?php echo get_the_ID(); ?>">
-                            <button type="submit" id="ash-skip-submit">Book This Skip</button>
-                        </form>
+            $query = new WP_Query( $args );
+            if ( $query->have_posts() ): while ( $query->have_posts() ):
+                $query->the_post(); ?>
+                <div class="ash-skip" id="ash-skip-<?php echo get_the_ID(); ?>">
+                    <h4 class="ash-skip-title"><?php the_title(); ?></h4>
+                    <p class="ash-skip-description"><?php echo get_post_meta( get_the_ID(), 'ash_skips_description', true ); ?></p>
+                    <div class="ash-skip__meta">
+                        <span class="ash-skip__meta--width">Width: <?php echo get_post_meta( get_the_ID(), 'ash_skips_width', true ); ?></span>
+                        <span class="ash-skip__meta--height">Height: <?php echo get_post_meta( get_the_ID(), 'ash_skips_height', true ); ?></span>
+                        <span class="ash-skip__meta--length">Length: <?php echo get_post_meta( get_the_ID(), 'ash_skips_length', true ); ?></span>
+                        <span class="ash-skip__meta--capacity">Capacity: <?php echo get_post_meta( get_the_ID(), 'ash_skips_capacity', true ); ?></span>
                     </div>
-                <?php
-                endwhile; endif;
-                wp_reset_postdata();
-                ?>
-            <?php else: ?>
-                <h3>Your Details</h3>
-                <form action="#">
-                    <!-- all of the skips here -->
 
-                    <!-- user details -->
+                    <p class="ash-skip__price">£<?php echo get_post_meta( get_the_ID(), 'ash_skips_price', true ); ?></p>
 
-                    <!-- delivery details -->
+                    <form action="#" method="POST" class="ash__form ash__form--skip">
+                        <input type="hidden" id="ash_postcode" name="ash_postcode" value="<?php echo $postcode; ?>">
+                        <input type="hidden" id="ash_skip_id" name="ash_skip_id" value="<?php echo get_the_ID(); ?>">
+                        <button type="submit" id="ash-skip-submit">Book This Skip</button>
+                    </form>
+                </div>
+            <?php
+            endwhile; endif;
+            wp_reset_postdata();
+            ?>
 
-                    <!-- permit, waste, notes -->
+        <?php elseif ( $skip != null ): ?>
 
-                    <!-- proceed to payment -->
-                </form>
-            <?php endif; ?>
-        </div>
-<?php
+            <h3>Complete Your Order</h3>
+            <p>Fill out your details and pick a delivery time to proceed.</p>
+
+            <form action="<?php echo get_permalink( get_page_by_title( 'Confirmation' ) ) ?>" method="POST">
+                <!-- user details -->
+                <fieldset class="ash__fieldset ash__fieldset-user">
+                    <legend class="ash__legend ash__legend-user">Your Details</legend>
+
+                    <div class="ash__input ash__input--title">
+                        <label for="ash_title">Title</label>
+                        <input type="text" name="ash_title" id="ash_title">
+                    </div>
+
+                    <div class="ash__input ash__input--forename">
+                        <label for="ash_forename">Forename</label>
+                        <input type="text" name="ash_forename" id="ash_forename">
+                    </div>
+
+                    <div class="ash__input ash__input--surname">
+                        <label for="ash_surname">Surname</label>
+                        <input type="text" name="ash_surname" id="ash_surname">
+                    </div>
+
+                    <div class="ash__input ash__input--email">
+                        <label for="ash_email">Email Address</label>
+                        <input type="email" name="ash_email" id="ash_email">
+                    </div>
+
+                    <div class="ash__input ash__input--phone">
+                        <label for="ash_phone">Phone Number</label>
+                        <input type="tel" name="ash_phone" id="ash_phone">
+                    </div>
+                </fieldset>
+
+                <!-- addresses -->
+                <fieldset class="ash__fieldset ash__fieldset-delivery">
+                    <legend class="ash__legend ash__legend-delivery">Delivery Address</legend>
+                    
+                    <div class="ash__input ash__input--address">
+                        <label for="ash_delivery_address_1">Address Line 1</label>
+                        <input type="tel" name="ash_delivery_address_1" id="ash_delivery_address_1">
+                    </div>
+
+                    <div class="ash__input ash__input--address">
+                        <label for="ash_delivery_address_2">Address Line 2</label>
+                        <input type="tel" name="ash_delivery_address_2" id="ash_delivery_address_2">
+                    </div>
+
+                    <div class="ash__input ash__input--city">
+                        <label for="ash_delivery_city">City</label>
+                        <input type="tel" name="ash_delivery_city" id="ash_delivery_city">
+                    </div>
+
+                    <div class="ash__input ash__input--county">
+                        <label for="ash_delivery_county">County</label>
+                        <input type="tel" name="ash_delivery_county" id="ash_delivery_county">
+                    </div>
+
+                    <div class="ash__input ash__input--postcode">
+                        <label for="ash_delivery_postcode">Post Code</label>
+                        <input type="tel" name="ash_delivery_postcode" id="ash_delivery_postcode">
+                    </div>
+
+                    <p class="ash__show-billing"><a href="javascript:void(0);">Is your billing address different?</a></p>
+                </fieldset>
+
+                <fieldset class="ash__fieldset ash__fieldset-billing">
+                    <legend class="ash__legend ash__legend-billing">Billing Address</legend>
+                    
+                    <div class="ash__input ash__input--address">
+                        <label for="ash_billing_address_1">Address Line 1</label>
+                        <input type="tel" name="ash_billing_address_1" id="ash_billing_address_1">
+                    </div>
+
+                    <div class="ash__input ash__input--address">
+                        <label for="ash_billing_address_2">Address Line 2</label>
+                        <input type="tel" name="ash_billing_address_2" id="ash_billing_address_2">
+                    </div>
+
+                    <div class="ash__input ash__input--city">
+                        <label for="ash_billing_city">City</label>
+                        <input type="tel" name="ash_billing_city" id="ash_billing_city">
+                    </div>
+
+                    <div class="ash__input ash__input--county">
+                        <label for="ash_billing_county">County</label>
+                        <input type="tel" name="ash_billing_county" id="ash_billing_county">
+                    </div>
+
+                    <div class="ash__input ash__input--postcode">
+                        <label for="ash_billing_postcode">Post Code</label>
+                        <input type="tel" name="ash_billing_postcode" id="ash_billing_postcode">
+                    </div>
+                </fieldset>
+
+                <fieldset class="ash__fieldset ash__fieldset-date">
+                    <legend class="ash__legend ash__legend-date">Delivery Date/Time</legend>
+
+                    <div class="ash__input ash__input--date">
+                        <label for="ash_delivery_date">Pick a Delivery Date</label>
+                        <input type="date" name="ash_delivery_date" id="ash_delivery_date" placeholder="dd/mm/yyyy">
+                    </div>
+
+                    <div class="ash__input ash__input--time">
+                        <span class="ash__fake-label">Pick a Time Slot</span>
+                        
+                        <input type="radio" class="ash__input--radio" id="ash_delivery_time[]" name="ash_delivery_am" value="AM"> <label for="ash_delivery_am">AM</label>
+                        <input type="radio" class="ash__input--radio" id="ash_delivery_time[]" name="ash_delivery_pm" value="PM"> <label for="ash_delivery_pm">PM</label>
+                    </div>
+                </fieldset>
+
+                <!-- permit, waste, notes -->
+                <fieldset class="ash__fieldset ash__fieldset-notes">
+                    <legend class="ash__legend ash__legend-notes">Notes</legend>
+
+                    <div class="ash__select ash__select--permit">
+                        <label for="ash_permit">Do You Need a Permit?</label>
+                        <select name="ash_permit" id="ash_permit">
+                            <option value="">No Permit Needed</option>
+                           <?php
+                            // query all of the permits available
+                            $args = [
+                                'post_type'              => 'ash_permits',
+                                'post_status'            => 'publish',
+                                'posts_per_page'         => -1,
+                                'cache_results'          => true,
+                            ];
+
+                            $query = new WP_Query( $args );
+                            if ( $query->have_posts() ): while ( $query->have_posts() ):
+                                $query->the_post(); ?>
+                            <option value="get_the_ID(); ?>"><?php echo get_the_title(); ?> (£<?php echo get_post_meta(get_the_ID(), 'ash_permits_price', true); ?>)</option>
+                            <?php endwhile; endif; wp_reset_postdata(); ?>
+                        </select>
+                    </div>
+
+                    <div class="ash__checkboxes ash__checkboxes--waste">
+                        <span class="ash__fake-label">Check all the types of waste you will be using the skip for</span>
+                        <div>
+                            <span class="ash__checkbox-wrapper"><input type="checkbox" name="ash_waste[]" id="ash_waste_concrete" class="ash__checkbox"> <label for="ash_waste_concrete">Concrete</label></span>
+                            <span class="ash__checkbox-wrapper"><input type="checkbox" name="ash_waste[]" id="ash_waste_metal" class="ash__checkbox"> <label for="ash_waste_metal">Metal</label></span>
+                            <span class="ash__checkbox-wrapper"><input type="checkbox" name="ash_waste[]" id="ash_waste_paper" class="ash__checkbox"> <label for="ash_waste_paper">Paper/Card</label></span>
+                            <span class="ash__checkbox-wrapper"><input type="checkbox" name="ash_waste[]" id="ash_waste_plastic" class="ash__checkbox"> <label for="ash_waste_plastic">Plastic</label></span>
+                            <span class="ash__checkbox-wrapper"><input type="checkbox" name="ash_waste[]" id="ash_waste_rubble" class="ash__checkbox"> <label for="ash_waste_rubble">Rubble/Brick</label></span>
+                            <span class="ash__checkbox-wrapper"><input type="checkbox" name="ash_waste[]" id="ash_waste_soil" class="ash__checkbox"> <label for="ash_waste_soil">Soil</label></span>
+                            <span class="ash__checkbox-wrapper"><input type="checkbox" name="ash_waste[]" id="ash_waste_wood" class="ash__checkbox"> <label for="ash_waste_wood">Wood</label></span>
+                            <span class="ash__checkbox-wrapper"><input type="checkbox" name="ash_waste[]" id="ash_waste_other" class="ash__checkbox"> <label for="ash_waste_other">Other</label></span>
+                        </div>
+                    </div>
+
+                    <div class="ash__textarea ash__textarea--notes">
+                        <label for="ash_notes">Additional Notes</label>
+                        <textarea name="ash_notes" id="ash_notes"></textarea>
+                    </div>
+                </fieldset>
+
+                <!-- proceed to confirmation -->
+                <button type="submit" class="ash__submit">Confirm Order</button>
+            </form>
+
+        <?php 
+        else:
+            echo "<p>Please enter a post code to see if we deliver to your area.</p>";
+            echo do_shortcode('[ash_postcode_form]');
+        endif;
     }
 }
