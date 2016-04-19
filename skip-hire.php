a19230c3696caaa1777ad4942b82c7687e74aed8
Jack Whiting <jack.whiting@adtrak.co.uk>
2016-04-19 11:42:27 +0100

    bumping to v0.3

--- skip-hire.php
+++ skip-hire.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Skip Hire
  * Plugin URI:        http://plugins.adtrakdev.com/skiphire
  * Description:       Adding the ability to hire skips and process payments within areas.
- * Version:           0.2.1
+ * Version:           0.3
  * Author:            Adtrak
  * Author URI:        http://adtrak.co.uk/
  * GitLab Theme URI:  https://gitlab.com/adtrak/skip-hire.git
