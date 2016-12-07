f9228aca7deb6003b7eb2d1442b0b2f120bf985f
Jack Whiting <hi@jackwhiting.co.uk>
2016-12-07 13:49:39 +0000

    setting up location dynamic search

--- app/Controllers/AdminController.php
+++ app/Controllers/AdminController.php
@@ -1,12 +1,13 @@
 <?php namespace Adtrak\Windscreens\Controllers;
 
 use Adtrak\Windscreens\View;
+use Billy\Framework\Facades\DB;
 use Adtrak\Windscreens\Models\Location;
 
 class AdminController
 {
 	private static $instance = null;
-	
+
 	public static function instance()
 	{
  		null === self::$instance and self::$instance = new self;
@@ -16,31 +17,53 @@ class AdminController
 	public function menu() 
 	{
 		add_menu_page(
-			__( 'Menu Page', 'textdomain' ),
-			'Menu Page',
+			__( 'Windscreens', 'adwind' ),
+			'Windscreens',
 			'manage_options',
-			'plugin-admin',
-			[$this, 'menu_render'],
+			'adwind',
+			'',
 			'',
 			100
+		);
+
+		add_submenu_page(
+			'adwind',			
+			__( 'Locations', 'adwind' ),
+			'Locations',
+			'manage_options',
+			'adwind',
+			[$this, 'menu_render'],
+			''
+		);
+
+		add_submenu_page(
+			'adwind',			
+			__( 'Settings', 'adwind' ),
+			'Settings',
+			'manage_options',
+			'adwind-settings',
+			[$this, 'menu_render'],
+			''
 		);
 	}
 
 	public function menu_render() 
 	{
 		$title = 'Hello From The Other Side';
-		$locations = Location::all();
 
+		$lat = '52.9539591';
+		$lng = '-1.1565018';
+		$radius = 4;
+
+		$locations = DB::table('aw_locations')
+						->select(DB::raw('id, name, lat, lng, radius, ( 3959 * acos( cos( radians(' . $lat . ') ) * cos( radians( lat ) ) * cos( radians( lng ) - radians(' . $lng . ') ) + sin( radians(' . $lat . ') ) * sin( radians( lat ) ) ) ) AS distance '))
+						->having('distance', '<', $radius)
+						->orderBy('distance')
+						->get();
+						
 		View::render('locations.twig', [
 			'title' 		=> $title,
 			'locations' 	=> $locations
 		]);
-	}
-
-	public function create_test_emails()
-	{
-		// Demo::create([
-		// 	'email' => 'example@test.com'
-		// ]);
 	}
 }
