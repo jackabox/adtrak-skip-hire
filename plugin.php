e96c3d5697d05f30e94727b2ab129255e192906a
Jack Whiting <hi@jackwhiting.co.uk>
2016-12-19 16:12:07 +0000

    raven only when debug disabled

--- plugin.php
+++ plugin.php
@@ -26,5 +26,7 @@ if (!defined('JB_PLUGIN_URL') ) {
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/vendor/getbilly/framework/bootstrap/autoload.php';
 
-// $client = new Raven_Client('https://b8752607c9f74cfcbc12681e3b50dedc:2ba1e8b0f0264760947658a89cfb0f33@sentry.io/122240');
-// $client->install();
+if (defined('WP_DEBUG') && false === WP_DEBUG) {
+	$client = new Raven_Client('https://b8752607c9f74cfcbc12681e3b50dedc:2ba1e8b0f0264760947658a89cfb0f33@sentry.io/122240');
+	$client->install();
+}
