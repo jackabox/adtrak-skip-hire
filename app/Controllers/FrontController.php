a1a8a642d260826d76f70dd27170f8cb6f5e5c4d
Jack Whiting <hi@jackwhiting.co.uk>
2016-12-20 11:27:00 +0000

    implemeting options for gmaps to front enqueue

--- app/Controllers/FrontController.php
+++ app/Controllers/FrontController.php
@@ -14,7 +14,8 @@ class FrontController
 
 	public function scripts()
 	{
+		wp_enqueue_script('maps-api', '//maps.googleapis.com/maps/api/js?key='. get_option('ash_google_maps_api', '') .'&libraries=places');
 		wp_enqueue_script('adtrak-skips', Helper::assetUrl('js/location.js'), ['jquery'], '', true);
-		wp_enqueue_script('maps-api', '//maps.googleapis.com/maps/api/js?key=AIzaSyDSIzp9xC7lMLnHSGj7WbSFYUDgNvkO02g&libraries=places');
+
 	}
 }
