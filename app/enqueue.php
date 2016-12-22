7b44b966dfd2f5dfef24c415fc56905dfb9b8445
Jack Whiting <hi@jackwhiting.co.uk>
2016-12-22 11:40:53 +0000

    adding enqueues in for admin

--- app/enqueue.php
+++ app/enqueue.php
@@ -1,6 +1,13 @@
-<?php 
-namespace Adtrak\Skips;
+<?php namespace Adtrak\Skips;
 
 /** @var \Billy\Framework\Enqueue $enqueue */
 
+$enqueue->admin([
+	'as' => 'adtrak-skips',
+	'src' => Helper::assetUrl('css/skips.css')
+]);
 
+$enqueue->admin([
+	'as' => 'adtrak-skips',
+	'src' => Helper::assetUrl('js/admin.js')
+], 'footer');
