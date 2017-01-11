32d225501b53092e42a2083fc1276086741fbd27
Jack Whiting <hi@jackwhiting.co.uk>
2017-01-11 15:36:37 +0000

    implementing front end styles

--- app/enqueue.php
+++ app/enqueue.php
@@ -11,3 +11,8 @@ $enqueue->admin([
 	'as' => 'chart-js',
 	'src' => '//cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js'
 ]);
+
+$enqueue->front([
+	'as' => 'adtrak-skip-frontend',
+	'src' => Helper::assetUrl('css/skip-frontend.css')
+]);
--- resources/templates/skips/loop.php
+++ resources/templates/skips/loop.php
@@ -15,33 +15,47 @@ if (!defined('ABSPATH')) exit; // Exit if accessed directly
 <?php foreach($skips as $skip): ?>
 
 	<div class="ash-skip__item">
-		<h4><?= $skip->name ?></h4>
 
-		<p>&pound;<?= $skip->price; ?></p>
-		
-		<?php if($skip->description): ?>
-			<p><?= $skip->description; ?></p>
+		<?php if($skip->image_url): ?>	
+			<div class="ash-skip__image">
+				<p><img src="<?= $skip->image_url ?>" alt="Image of <?= $skip->name ?>"></p>
+			</div>
+			<div class="ash-skip__content ash-skip__content--float">	
+		<?php else: ?>
+			<div class="ash-skip__content">
 		<?php endif; ?>
 
-		<p>
-			<?php if($skip->width): ?>
-				<span><b>Width:</b> <?= $skip->width ?>m</span>
-			<?php endif; ?>
+			<h4><?= $skip->name ?></h4>
 
-			<?php if($skip->height): ?>
-				<span><b>Height:</b> <?= $skip->height ?>m</span>
-			<?php endif; ?>
-
-			<?php if($skip->length): ?>
-				<span><b>Length:</b> <?= $skip->length ?>m</span>
-			<?php endif; ?>
+			<p>&pound;<?= $skip->price; ?></p>
 			
-			<?php if($skip->capacity): ?>
-				<span><b>Capacity:</b> <?= $skip->capacity ?></span>
+			<?php if($skip->description): ?>
+				<p><?= $skip->description; ?></p>
 			<?php endif; ?>
-		</p>
 
-		<p><a href="#" class="ash-button">Purchase</a></p>
+			<p class="ask-skip__meta">
+				<?php if($skip->width): ?>
+					<span><b>Width:</b> <?= $skip->width ?>m</span>
+				<?php endif; ?>
+
+				<?php if($skip->height): ?>
+					<span><b>Height:</b> <?= $skip->height ?>m</span>
+				<?php endif; ?>
+
+				<?php if($skip->length): ?>
+					<span><b>Length:</b> <?= $skip->length ?>m</span>
+				<?php endif; ?>
+				
+				<?php if($skip->capacity): ?>
+					<span><b>Capacity:</b> <?= $skip->capacity ?></span>
+				<?php endif; ?>
+			</p>
+
+			<form action="<?= site_url('checkout'); ?>" action="POST">
+				<input type="hidden" val="<?= $skip->id; ?>">
+				<p><button type="submit" class="ash-button">Purchase</button></p>
+			</form>
+		</div>
 	</div>
 
 <?php endforeach; ?>
