edaedc3e61227ed600f1d694fb75cd3db3f4e04b
Jack Whiting <hi@jackwhiting.co.uk>
2017-01-24 15:29:19 +0000

    Skip Overview Changes [References #10]
    - Implementing order counts for 31days/lifetime
    - Implementing revenue counts for 31days/lifetime
    - Added overview of outstanding orders.

--- app/Controllers/Admin/DashboardController.php
+++ app/Controllers/Admin/DashboardController.php
@@ -1,7 +1,10 @@
-<?php namespace Adtrak\Skips\Controllers\Admin;
+<?php
+
+namespace Adtrak\Skips\Controllers\Admin;
 
 use Adtrak\Skips\Facades\Admin;
 use Adtrak\Skips\View;
+use Adtrak\Skips\Models\Order;
 
 class DashboardController extends Admin
 {
@@ -27,6 +30,21 @@ class DashboardController extends Admin
      */
     public function index()
 	{
-		View::render('admin/dashboard.twig', []);
+        // last week / month  
+        $lastMonth = date('Y-m-d', strtotime(date('Y-m-d') . "-1 month"));
+
+        // get order counts
+        $stats['orders_lifetime'] = Order::count();
+        $stats['revenue_lifetime'] = Order::sum('total');
+        $stats['orders_month'] = Order::where('created_at', '>=', $lastMonth)->count();
+        $stats['revenue_month'] = Order::where('created_at', '>=', $lastMonth)->sum('total');
+       
+        // get orders (not delivered)
+        $orders = Order::where('status', '!=', 'pending')->get();
+
+		View::render('admin/dashboard.twig', [
+            'orders'  => $orders,
+            'stats' => $stats,
+        ]);
 	}
 }
