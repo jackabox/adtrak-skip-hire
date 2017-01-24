da65a2103f1362b5f3e5e101dff656c1e5cb42eb
Jack Whiting <hi@jackwhiting.co.uk>
2017-01-24 11:02:26 +0000

    fail message tweaks

--- resources/templates/confirmation/fail.php
+++ resources/templates/confirmation/fail.php
@@ -1,3 +1,3 @@
 <h1>Fail</h1>
-<p>Our apologies, the payment could not be accepted. {{ order number }} {{ date }} {{ skip }}</p>
+<p>Oops, something went wrong with processing your payment. Please go back and try again.</p>
 <p>If you have any enquiries feel free to contact us on {{ contact number }}</p>
