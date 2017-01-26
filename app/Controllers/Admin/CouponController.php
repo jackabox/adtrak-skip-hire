cf5a802623c502c99b2ae6dd665f425aad848071
Jack Whiting <hi@jackwhiting.co.uk>
2017-01-26 12:54:18 +0000

    updating admin coupons
    - updating structure
    - cleaning up code
    - adding fail loops
    - adding comments

--- app/Controllers/Admin/CouponController.php
+++ app/Controllers/Admin/CouponController.php
@@ -1,9 +1,14 @@
-<?php namespace Adtrak\Skips\Controllers\Admin;
+<?php
+namespace Adtrak\Skips\Controllers\Admin;
 
 use Adtrak\Skips\Facades\Admin;
 use Adtrak\Skips\Models\Coupon;
 use Adtrak\Skips\View;
 
+/**
+ * Class CouponController
+ * @package Adtrak\Skips\Controllers\Admin
+ */
 class CouponController extends Admin
 {
 
@@ -27,20 +32,24 @@ class CouponController extends Admin
 	}
 
     /**
-     *
+     * @return mixed
      */
     public function index()
 	{
-		// work out if pages
+		# work out if pages
 		$limit = 16;		
 		$pagenum = isset($_GET['pagenum']) ? absint($_GET['pagenum']) : 1;
 		$offset = $limit * ($pagenum - 1);
 		$total = Coupon::count();
 		$totalPages = ceil($total / $limit);
 
-		// get results
-		$coupons = Coupon::orderBy('created_at', 'desc')->skip($offset)->take($limit)->get();
+		# get results
+		$coupons = Coupon::orderBy('created_at', 'desc')
+                        ->skip($offset)
+                        ->take($limit)
+                        ->get();
 
+		# pagination using WP in built function
 		$pagination = paginate_links( array(
     		'base'      => add_query_arg( 'pagenum', '%#%' ),
     		'format'    => '',
@@ -49,13 +58,15 @@ class CouponController extends Admin
     		'total'     => $totalPages,
     		'current'   => $pagenum
 		));
-		
+
+		# Links for edit/add
 		$link = [
 			'edit' => admin_url('admin.php?page=ash-coupons-edit&id='),
 			'add'  => admin_url('admin.php?page=ash-coupons-add')
 		];
 
-		View::render('admin/coupons/index.twig', [
+		# render the view
+		return View::render('coupons/index.twig', [
 			'coupons'    => $coupons,
 			'link'       => $link,
 			'pagination' => $pagination
@@ -63,7 +74,7 @@ class CouponController extends Admin
 	}
 
     /**
-     *
+     * @return mixed
      */
     public function create()
 	{
@@ -71,7 +82,7 @@ class CouponController extends Admin
 			$this->store();
 		}
 
-		View::render('admin/coupons/add.twig', []);
+		return View::render('coupons/add.twig', []);
 	}
 
     /**
@@ -79,9 +90,8 @@ class CouponController extends Admin
      */
     public function store()
 	{
-		// $permission = wp_verify_nonce($_GET['nonce'], 'coupon_add_nonce');
 		$permission = true;
-		$errors 	= [];
+		$errors = [];
 
 		if (empty($_REQUEST['code'])) {
 			$errors[] = 'Please enter a name.';
@@ -89,7 +99,7 @@ class CouponController extends Admin
 
         if ($permission === false) {
             echo 'Permission Denied';
-        } else if (!empty($errors)) {
+        } else if (! empty($errors)) {
 			echo '<ul>';
 			foreach($errors as $error) {
 				echo '<li>' . $error . '</li>';
@@ -110,13 +120,13 @@ class CouponController extends Admin
 				echo '<script>window.location.href=' . $url . ';</script>';
 				die();
 			} catch (Exception $e) {
-				 echo 'Caught exception: ',  $e->getMessage(), "\n";
+				echo 'Caught exception: ',  $e->getMessage(), "\n";
 			}
         }
 	}
 
     /**
-     *
+     * @return mixed
      */
     public function edit()
 	{
@@ -126,21 +136,17 @@ class CouponController extends Admin
 		
  		if (current_user_can('delete_posts')) {
 			$nonce = wp_create_nonce('ash_coupon_delete_nonce');
-			$button['delete'] = 'or <a href="' . admin_url( 'admin-ajax.php?action=ash_coupon_delete&id=' . $_GET['id'] . '&nonce=' . $nonce ) . '" data-id="' . $_GET['id'] . '" data-nonce="' . $nonce . '" data-redirect="' . admin_url('admin.php?page=ash-coupons') . '" class="ash-coupon-delete">Delete</a>';
+			$delete = 'or <a href="' . admin_url( 'admin-ajax.php?action=ash_coupon_delete&id=' . $_GET['id'] . '&nonce=' . $nonce ) . '" data-id="' . $_GET['id'] . '" data-nonce="' . $nonce . '" data-redirect="' . admin_url('admin.php?page=ash-coupons') . '" class="ash-coupon-delete">Delete</a>';
 		} else {
-			$button['delete'] = '';
+			$delete = '';
 		}
 
-		$coupon = Coupon::find($_GET['id']);
+		$coupon = Coupon::findOrFail($_GET['id']);
 
-		if ($coupon) {
-			View::render('admin/coupons/edit.twig', [
-				'coupon' 	=> $coupon,
-				'button'	=> $button
-			]);
-		} else {
-			echo "Sorry, the coupon you're looking for does not exist.";
-		}
+        return View::render('coupons/edit.twig', [
+            'coupon' 	=> $coupon,
+            'delete'	=> $delete
+        ]);
 	}
 
     /**
@@ -150,7 +156,7 @@ class CouponController extends Admin
 	{
 		// $permission = wp_verify_nonce($_GET['nonce'], 'coupon_add_nonce');
 		$permission = true;
-		$errors 	= [];
+		$errors = [];
 
 		if (empty($_REQUEST['code'])) {
 			$errors[] = 'Please enter a name.';
@@ -176,7 +182,7 @@ class CouponController extends Admin
 
 				echo "Coupon has been updated";
 			} catch (Exception $e) {
-				 echo 'Caught exception: ',  $e->getMessage(), "\n";
+				echo 'Caught exception: ',  $e->getMessage(), "\n";
 			}
         }
 	}
